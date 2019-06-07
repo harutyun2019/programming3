6c95bef5a16e8e4eacd9400d7c1d8f94e712ec31
harutyun2019 <harutyun.h1.y@tumo.org>
2019-06-07 17:15:36 +0400

    nor

--- Kaban.js
+++ Kaban.js
@@ -1,57 +1,34 @@
-class Kaban {
-    constructor(x, y, index) {
-        this.x = x;
-        this.y = y;
-        this.index = index;
-        this.energy = 10;
+
+class Kaban extends LivingCreature {
+    constructor(x, y, index){
+        super(x, y, index);
+        this.energy = 8;
     }
-    //vorpes method
-    getNewCoordinates() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1],
+   getNewCoordinates() {
+       this.directions = [
+           [this.x - 1, this.y - 1],
+           [this.x, this.y - 1],
+           [this.x + 1, this.y - 1],
+           [this.x - 1, this.y],
+           [this.x + 1, this.y],
+           [this.x - 1, this.y + 1],
+           [this.x, this.y + 1],
+           [this.x + 1, this.y + 1]
 
-            [this.x - 2, this.y - 2],
-            [this.x, this.y - 2],
-            [this.x + 2, this.y - 2],
-            [this.x - 2, this.y],
-            [this.x + 2, this.y],
-            [this.x - 2, this.y + 2],
-            [this.x, this.y + 2],
-            [this.x + 2, this.y + 2],
-
-            [this.x - 3, this.y - 3],
-            [this.x, this.y - 3],
-            [this.x + 3, this.y - 3],
-            [this.x - 3, this.y],
-            [this.x + 3, this.y],
-            [this.x - 3, this.y + 3],
-            [this.x, this.y + 3],
-            [this.x + 3, this.y + 3],
-        ];
-    }
-
-    chooseCell(character) {
-        this.getNewCoordinates();
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-
-                }
-            }
-        }
-        return found;
-    }
+           [this.x - 1, this.y - 1],
+           [this.x, this.y - 1],
+           [this.x + 1, this.y - 1],
+           [this.x - 1, this.y],
+           [this.x + 1, this.y],
+           [this.x - 1, this.y + 1],
+           [this.x, this.y + 1],
+           [this.x + 1, this.y + 1]
+       ];
+   }
+   chooseCell(character) {
+       this.getNewCoordinates(4);
+       return super.chooseCell(character);
+   }
     //qayluma
     move() {
 
